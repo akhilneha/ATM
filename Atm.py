16c5aa30f9cf5d821c8119481c0d3bbf4804e127
akhilneha <90970355+akhilneha@users.noreply.github.com>
2022-01-22 15:27:01 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class atm(object):
+    def_init_(self,acnumber,pinnumber):
+        self.acnumber=account number
+        self.pinnumber=pin number
+
+    def CashWithdrawl(self):
+        print("Cash has been taken")
+
+    def BalanceEnquiry(self):
+        print("Balance has been checked")
